58dd0a57fb892fcc7ab133344174cda79e86db49
AtamanKit <rodionatamaniuc@gmail.com>
2024-01-23 18:25:26 +0200

    Added tests

--- app/config.py
+++ app/config.py
@@ -4,10 +4,10 @@ class Config:
     DB_CONFIG = os.getenv(
         "DB_CONFIG",
         "postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}".format(
-            DB_USER=os.getenv("DB_USER", "fastapi"),
-            DB_PASSWORD=os.getenv("DB_PASSWORD", "fastapi-password"),
-            DB_HOST=os.getenv("DB_HOST", "fastapi-postgresql:5432"),
-            DB_NAME=os.getenv("DB_NAME", "fastapi"),
+            DB_USER=os.getenv("DB_USER", "postgres"),
+            DB_PASSWORD=os.getenv("DB_PASSWORD", "postgres1234"),
+            DB_HOST=os.getenv("DB_HOST", "localhost"),
+            DB_NAME=os.getenv("DB_NAME", "postgres"),
         ),
     )
 
